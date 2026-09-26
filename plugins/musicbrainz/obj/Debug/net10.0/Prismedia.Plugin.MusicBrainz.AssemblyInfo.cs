7dc044a32f3363e51d1665117782d46b0145d2a5
Paul Davis <pauldavis101@gmail.com>
2026-09-26 17:12:21 -0500

    Remove junk

--- plugins/musicbrainz/obj/Debug/net10.0/Prismedia.Plugin.MusicBrainz.AssemblyInfo.cs
+++ plugins/musicbrainz/obj/Debug/net10.0/Prismedia.Plugin.MusicBrainz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prismedia.Plugin.MusicBrainz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fb2af6b041e256c9e66cc3a51f3579426108c75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27137281d4d46d3e71e9cd704b4d8c06f173b4b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prismedia.Plugin.MusicBrainz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prismedia.Plugin.MusicBrainz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
